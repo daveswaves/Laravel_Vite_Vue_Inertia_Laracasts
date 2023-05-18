a28cad5c73023a8f6d2195aefa0d55adc5c9dc3b
David <daveswaves.2008@gmail.com>
2023-05-18 20:38:26 +0100

    Using Middleware To Share Username Data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -3,13 +3,30 @@ import Nav from "./Nav.vue";
 
 export default {
   components: { Nav },
+  
+  computed: {
+    /*
+      username() returns the namespaced 'username' value,
+      set in the 'Middleware/HandleInertiaRequests.php' file.
+      Doing so makes it globally available to all pages within the project.
+      Nb. We could reference the value directly like so ('this' not required):
+      {{ $page.props.auth.user.username }}
+    */
+    username() {
+      return this.$page.props.auth.user.username;
+    }
+  }
 };
 </script>
 
 <template>
   <section class="p-6 bg-gray-200">
     <header class="flex justify-between">
-      <h1 class="font-bold text-lg">My App</h1>
+      <div class="flex items-center">
+        <h1 class="font-bold text-lg">My App</h1>
+
+        <p class="text-sm ml-4">{{ username }}</p>
+      </div>
       <Nav />
     </header>
   </section>
